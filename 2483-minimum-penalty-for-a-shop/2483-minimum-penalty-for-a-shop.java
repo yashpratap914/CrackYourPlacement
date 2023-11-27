1cd2261beab1261b4655036c474380127b664585
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2023-11-27 23:51:52 +0530

    Time: 8 ms (95.67%), Space: 43.5 MB (99.11%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int bestClosingTime(String customers) {
+        int maxScore = 0;
+        int bestIndex = 0;
+        int currentScore = 0;
+        for (int i = 0; i < customers.length(); ++i) {
+            char customerShowedUp = customers.charAt(i);
+            if (customerShowedUp == 'Y') {
+                ++currentScore;
+            } else {
+                --currentScore;
+            }
+        if (maxScore < currentScore) {
+            maxScore = currentScore;
+            bestIndex = i + 1;
+        }
+        }
+        return bestIndex;
+
+        
+    }
+}
