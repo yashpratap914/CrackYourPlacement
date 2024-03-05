86a9b8db88dd87d4ebbd93faa91ad610efb1a60f
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-03-05 23:34:31 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (18.46%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,19 +1,20 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m-1;
-        int p2 = n-1;
-        for(int i = nums1.length-1; i>=0; i--){
-            int valatp1=p1>=0?nums1[p1]: Integer.MIN_VALUE;
-            int valatp2 = p2>=0?nums2[p2]: Integer.MIN_VALUE;
-            if(valatp1>valatp2){
-                nums1[i]= valatp1;
-                p1--;
-            } else{
-                nums1[i]= valatp2;
-                p2--;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
             }
-            
         }
-        //TC-O(M+N)  SC-O(1)
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+        
     }
 }
