ad839e70c8d55ea0839988d5f4f254eafbd9586b
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-06-05 02:46:03 +0530

    Time: 2 ms (62.51%), Space: 46 MB (28.80%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,52 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-
-        if (m > n) {
-            return findMedianSortedArrays(nums2, nums1);
+        int[] ans = merge(nums1,nums2);
+        
+        if(ans.length%2==0){
+            double ans2 = (double) (ans[ans.length/2]+ans[(ans.length/2)-1])/2;
+            return ans2;
+            
         }
-
-        int total = m + n;
-        int half = (total + 1) / 2;
-
-        int left = 0;
-        int right = m;
-
-        var result = 0.0;
-
-        while (left <= right) {
-            int i = left + (right - left) / 2;
-            int j = half - i;
-
-            // get the four points around possible median
-            int left1 = (i > 0) ? nums1[i - 1] : Integer.MIN_VALUE;
-            int right1 = (i < m) ? nums1[i] : Integer.MAX_VALUE;
-            int left2 = (j > 0) ? nums2[j - 1] : Integer.MIN_VALUE;
-            int right2 = (j < n) ? nums2[j] : Integer.MAX_VALUE;
-
-            // partition is correct
-            if (left1 <= right2 && left2 <= right1) {
-                // even
-                if (total % 2 == 0) {
-                    result =
-                        (Math.max(left1, left2) + Math.min(right1, right2)) /
-                        2.0;
-                    // odd
-                } else {
-                    result = Math.max(left1, left2);
-                }
-                break;
-            }
-            // partition is wrong (update left/right pointers)
-            else if (left1 > right2) {
-                right = i - 1;
-            } else {
-                left = i + 1;
-            }
+        else{
+            double ans2 = (double) (ans[ans.length/2]);
+            return ans2;
         }
-
-        return result;
         
     }
+    
+    public int[] merge(int[] arr1, int[] arr2){
+        
+        int[] ans = new int[arr1.length +  arr2.length];
+        
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        
+        while(p1<arr1.length || p2<arr2.length){
+            int val1 = p1<arr1.length?arr1[p1]:Integer.MAX_VALUE;
+            int val2 = p2<arr2.length?arr2[p2]:Integer.MAX_VALUE;
+        
+            if(val1<val2){
+                ans[p3]=val1;
+                p1++;
+            }
+            else{
+                ans[p3]=val2;
+                p2++;
+            }
+            p3++;
+        }
+        return ans;
+        
+    }
+    
 }
