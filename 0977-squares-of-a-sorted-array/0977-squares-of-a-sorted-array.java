c236c59fab47392ce4b27d1e2e6cd202e8cd167e
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-06-08 13:26:24 +0530

    Time: 1 ms (100.00%), Space: 47.4 MB (21.11%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,18 +1,26 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int left=0, right= nums.length-1;
-        int[] squared = new int[nums.length];
-        int index = squared.length-1;
-        while(left<=right){
-            int tmp= Math.max(nums[left]*nums[left], nums[right]*nums[right]);
-            squared[index--]=tmp;
-            if(Math.abs(nums[left]) < Math.abs(nums[right])){
-                right--;
-            }else{
-                left++;
+        int[] ans = new int[nums.length];
+        int st = 0;
+        int end = nums.length-1;
+        int ptr = ans.length-1;
+        
+        while(end>=st){
+            if((nums[st]*nums[st]) > (nums[end]*nums[end])){
+                ans[ptr] = nums[st]*nums[st];
+                st++;
+                
             }
+            else{
+                ans[ptr] = nums[end]*nums[end];
+                end--;
+            }
+            
+            ptr--;
         }
-        return squared;
-        //TC-O(n) AND SC-O(n)
+        return ans; 
+        
+        
+        
     }
 }
