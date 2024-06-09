2d41db18dd28cabf1327cb4b4d3fe808769658ac
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-06-09 17:09:49 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (81.68%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,20 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left=0;
-        int right = nums.length-1;
-        while(left <= right){
-            int mid = left + (right-left)/2;
-            if(nums[mid]==target){
-                return mid;
+        int st =0;
+        int end = nums.length - 1;
+        int mid =0;
+        while(st<=end){
+            mid = st + (end-st) /2;
+            if(target<nums[mid]){
+                end = mid-1; 
             }
-            if(target>nums[mid]){
-                left= mid+1;
+            else if(target>nums[mid]){
+                st = mid+1;
             }
             else{
-                right = mid-1;
+                return mid;
             }
         }
         return -1;
         
     }
-}// TC-O(log n)
+}
