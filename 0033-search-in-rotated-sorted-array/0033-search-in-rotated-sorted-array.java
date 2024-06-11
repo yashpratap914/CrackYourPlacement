db738ec8af038361621d8bb006ffd5ea41ed322f
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-06-12 04:52:33 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (84.83%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int st =0;
+        int end = nums.length -1;
+        
+        while(st<=end){
+            int mid = st + (end - st)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[st]<=nums[mid]){
+                if(target< nums[mid] && target>=nums[st]){
+                    end = mid-1;
+                }
+                else{
+                    st = mid+1;
+                }
+                
+            }
+            else{
+                if(target> nums[mid] && target<=nums[end]){
+                    st = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
