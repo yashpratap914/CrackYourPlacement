4082dec8f263e0c9cd11d83f233c9171dda1af33
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-06-13 02:10:12 +0530

    Time: 1 ms (70.16%), Space: 45.5 MB (88.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,48 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        if (height == null || height.length == 0) {
-            return 0;
+        int n = height.length;
+        int[] leftmax  = new int[n];
+        leftmax[0] = height[0];
+        for(int i=1; i<n; i++){
+            leftmax[i] = Math.max(leftmax[i-1],height[i]);
         }
-        int left = 0; int right = height.length - 1; // Pointers to both ends of the array.
-        int maxLeft = 0; int maxRight = 0;
+        int[] rightmax = new int[n];
+        rightmax[n-1] = height[n-1];
         
-        int totalWater = 0;
-        while (left < right) {
-            // Water could, potentially, fill everything from left to right, if there is nothing in between.
-            if (height[left] < height[right]) {
-                // If the current elevation is greater than the previous maximum, water cannot occupy that point at all.
-                // However, we do know that everything from maxLeft to the current index, has been optimally filled, as we've
-                // been adding water to the brim of the last maxLeft.
-                if (height[left] >= maxLeft) { 
-                    // So, we say we've found a new maximum, and look to see how much water we can fill from this point on.
-                    maxLeft = height[left]; 
-                // If we've yet to find a maximum, we know that we can fill the current point with water up to the previous
-                // maximum, as any more will overflow it. We also subtract the current height, as that is the elevation the
-                // ground will be at.
-                } else { 
-                    totalWater += maxLeft - height[left]; 
-                }
-                // Increment left, we'll now look at the next point.
-                left++;
-            // If the height at the left is NOT greater than height at the right, we cannot fill from left to right without over-
-            // flowing; however, we do know that we could potentially fill from right to left, if there is nothing in between.
-            } else {
-                // Similarly to above, we see that we've found a height greater than the max, and cannot fill it whatsoever, but
-                // everything before is optimally filled
-                if (height[right] >= maxRight) { 
-                    // We can say we've found a new maximum and move on.  
-                    maxRight = height[right]; 
-                // If we haven't found a greater elevation, we can fill the current elevation with maxRight - height[right]
-                // water.
-                } else {
-                    totalWater += maxRight - height[right]; 
-                }
-                // Decrement left, we'll look at the next point.
-                right--;
-            }
+        for(int i=n-2; i>=0; i--){
+            rightmax[i] = Math.max(height[i], rightmax[i+1]);
         }
-        // Return the sum we've been adding to.
-        return totalWater;
+        int ans=0;
+        
+        for(int i=0; i<n; i++){
+            ans += Math.min(leftmax[i], rightmax[i])-height[i];
+        }
+        return ans;
+        
         
     }
 }
