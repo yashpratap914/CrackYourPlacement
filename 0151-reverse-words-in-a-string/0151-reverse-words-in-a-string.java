fd81b8a5057385def353391384d74aad85b91617
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-06-17 21:24:48 +0530

    Time: 9 ms (44.17%), Space: 43.1 MB (57.10%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,52 +1,18 @@
-public class Solution {
+class Solution {
     public String reverseWords(String s) {
-        char[] str = s.toCharArray();
-        // step 1: reverse the entire string
-        reverse(str, 0, str.length - 1);
-        // step 2: reverse each words
-        reverseWords(str);
-        // step 3: clean spaces
-        return cleanSpaces(str);
-    }
-    
-    private String cleanSpaces(char[] str) {
-        int left = 0;
-        int right = 0;
-        while (right < str.length) {
-            while (right < str.length && str[right] == ' ') right++;
-            while (right < str.length && str[right] != ' ') {
-                str[left] = str[right];
-                left++;
-                right++;
-            }
-            while (right < str.length && str[right] == ' ') right++;
-            if (right < str.length) {
-                str[left] = ' ';
-                left++;
-            }
-        } // 0 to left - 1
-        return new String(str).substring(0, left);
-    }
-    
-    private void reverseWords(char[] str) {
-        int left = 0;
-        int right = 0;
-        while (left < str.length) {
-            while (left < str.length && str[left] == ' ') left++;
-            right = left;
-            while (right < str.length && str[right] != ' ') right++;
-            reverse(str, left, right - 1);
-            left = right;
+        String[] w = s.split("\\s+"); // split on basis of spaces
+        StringBuilder sb = new StringBuilder(""); // gain n again +
+        
+        for(int i = w.length-1; i>=0;i--){
+            
+            sb.append(w[i]+" ");
         }
-    }
-    
-    private void reverse(char[] str, int left, int right) {
-        while (left < right) {
-            char tmp = str[left];
-            str[left] = str[right];
-            str[right] = tmp;
-            left++;
-            right--;
-        }
+        
+        //trim func -> remove trailing nd leading spaces
+        String ans = sb.toString();
+        ans = ans.trim();
+        
+        return ans;
+        
     }
 }
