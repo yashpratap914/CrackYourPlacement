eb429c6ce8fa484ae6373a3d271ed141e393c4e4
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-09 05:28:30 +0530

    Time: 1 ms (100.00%), Space: 45.6 MB (89.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,43 +1,44 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] ans = merge(nums1,nums2);
+        //num1 -> smaller(assume)
         
-        if(ans.length%2==0){
-            double ans2 = (double) (ans[ans.length/2]+ans[(ans.length/2)-1])/2;
-            return ans2;
+        if(nums2.length<nums1.length){
+            return findMedianSortedArrays(nums2, nums1);
+        } //used recursion
+        
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int N = n1+n2;
+        int st =0;
+        int end = n1;
+        
+        while(st<=end){
+            int cut1 = st + (end - st)/2; //mid
+            //cut2 = (total/2) - cut1 here we will be making it on second array as a result of our requirement
+            int cut2 = (N/2) - cut1;
+            //cut2 is cut in second array
+            int l1 = (cut1==0)?Integer.MIN_VALUE:nums1[cut1-1];
+            int l2 = (cut2==0)?Integer.MIN_VALUE:nums2[cut2-1];
+            int r1 = (cut1 == n1)?Integer.MAX_VALUE:nums1[cut1];
+            int r2 = (cut2 == n2)?Integer.MAX_VALUE:nums2[cut2];
+
+            
+            // valid cut or not
+            if(l1<=r2 && l2<=r1){
+                if(N%2==0){  // even number case median
+                    return ((Math.max(l1,l2)+Math.min(r1,r2))/2.0);
+                }else{  // odd number case median
+                    return (double) Math.min(r1,r2);
+                }
+            }else if(l1>r2){
+                end = cut1 -1;
+            }else{
+                st = cut1 + 1;
+            }
+            
             
         }
-        else{
-            double ans2 = (double) (ans[ans.length/2]);
-            return ans2;
-        }
+        return 0.0;
         
     }
-    
-    public int[] merge(int[] arr1, int[] arr2){
-        
-        int[] ans = new int[arr1.length +  arr2.length];
-        
-        int p1 = 0;
-        int p2 = 0;
-        int p3 = 0;
-        
-        while(p1<arr1.length || p2<arr2.length){
-            int val1 = p1<arr1.length?arr1[p1]:Integer.MAX_VALUE;
-            int val2 = p2<arr2.length?arr2[p2]:Integer.MAX_VALUE;
-        
-            if(val1<val2){
-                ans[p3]=val1;
-                p1++;
-            }
-            else{
-                ans[p3]=val2;
-                p2++;
-            }
-            p3++;
-        }
-        return ans;
-        
-    }
-    
 }
