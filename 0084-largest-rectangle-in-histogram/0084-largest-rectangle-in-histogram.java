f86135328d8569ed18e612a0da4a761f400079c2
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-18 23:38:54 +0530

    Time: 116 ms (17.01%), Space: 57.4 MB (41.94%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,19 +1,52 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int maxArea =0;
+        //height(given) ans width (next smaller and previous smaller) -> next smaller array and prev smaller array
+        //width = upper lim - lower lim - 1
+        int[] ns = findNextSmaller(heights);
+        int[] ps = findPrevSmaller(heights);
+        
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<heights.length; i++){
+            int h = heights[i];
+            int w = ns[i] - ps[i] - 1;
+            max = Math.max(max, (h*w));
+        }
+        return max;        
+    }
+    public int[] findPrevSmaller(int[] arr){
+        int n = arr.length;
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        for ( int i=1; i<= heights.length; i++){
-            int curr = (i == heights.length)? -1 : heights[i];
-            while(!stack.isEmpty() && curr <= heights[stack.peek()])            {
-                int height = heights[stack.pop()];
-                int width = stack.isEmpty()?i:i-stack.peek()-1;
-                int currArea = height*width;
-                maxArea= Math.max(maxArea, height*width);
+        int res[] = new int[n];
+        for(int i = 0; i<n; i++){
+            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
+                stack.pop();
             }
+            if(stack.isEmpty()){
+                res[i] = -1;
+            }else{
+                res[i] = stack.peek();
+            }
+            
             stack.push(i);
         }
-        return maxArea;
-        
+        return res;
     }
-}//TC: O(n), SC:O(n)
+    public int[] findNextSmaller(int[] arr){
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        int res[] = new int[n];
+        for(int i = n-1; i>=0; i--){
+            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                res[i] = n;
+            }else{
+                res[i] = stack.peek();
+            }
+            
+            stack.push(i);
+        }
+        return res;
+    }
+}
