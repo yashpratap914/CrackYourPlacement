f1d5e6cbe6c938441250d90f69851ec2a2012e41
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-19 09:52:48 +0530

    Time: 4 ms (99.56%), Space: 45.1 MB (72.83%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,24 +1,30 @@
 class Solution {
-    public boolean validPalindrome(String s) {
-         int i = 0, j = s.length() - 1;
-        while(i < j)
-            if(s.charAt(i) == s.charAt(j)) {
-                i += 1;
-                j -= 1;
-            }else
-                return validPalindromeUtil(s, i + 1, j) || validPalindromeUtil(s, i, j - 1);
+    public boolean ispalindrome(String s, int i, int j){
+         while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
         return true;
         
     }
-    boolean validPalindromeUtil(String s, int i, int j) {
-        while(i < j)
-            if(s.charAt(i) == s.charAt(j)) {
-                i += 1;
-                j -= 1;
-            } else {
-                return false;
+    public boolean validPalindrome(String s) {
+        int i=0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
             }
-        
+            else{
+                return ispalindrome(s,i+1,j)||ispalindrome(s,i,j-1);
+            }
+        }
         return true;
+        
     }
 }
