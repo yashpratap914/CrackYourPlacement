bcf59e9ac89b6c0664fb2ecbdb54b9980e92e75f
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-20 13:04:44 +0530

    Time: 7 ms (69.25%), Space: 47.8 MB (47.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,16 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        //multiple anagram based groups
+        // sort the string in groups
         Map<String, List<String>> map = new HashMap<>();
-        for (String word : strs){
-            char[] ca = word.toCharArray();
-            Arrays.sort(ca);
-            String str = new String(ca);
-            if(!map.containsKey(str))
-                map.put(str, new ArrayList<>());
-            map.get(str).add(word);
+        for(String s : strs){
+            //Sorting: we cannot directly sort the string but we can make char array
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray); // sorted
+            String sorted = new String(charArray); // comparator of string class -> key
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new LinkedList<String>()); // put new sorted key in map
+            }
+            map.get(sorted).add(s); //LL mei add krdiya if key is same
             
         }
-        return new ArrayList<>(map.values());
-    //TC - O(n klogk) SC- O(nk)   
+        return new LinkedList<>(map.values());  
+        
+        
     }
 }
