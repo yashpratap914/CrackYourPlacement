443ad0ac524878b458ec2988d6e372f14a3d0d65
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-23 10:43:16 +0530

    Time: 1 ms (93.77%), Space: 44.9 MB (9.14%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,54 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums);
-        return result;      
+        // Create a list to hold all the permutations of the array
+        List<List<Integer>> l = new ArrayList<>();
+        
+        // Start the backtracking process from the first index (0)
+        backtrack(l, nums, 0);
+        
+        // Return the list of permutations
+        return l;
     }
     
-    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
-        if (tempList.size() == nums.length) {
-            result.add(new ArrayList<>(tempList));
+    // Recursive method to generate permutations using backtracking
+    public void backtrack(List<List<Integer>> l, int[] nums, int start) {
+        // Base case: if the start index is equal to the length of the array
+        // it means a full permutation has been generated
+        if (start == nums.length) {
+            // Convert the current permutation (array) to a list and add to the result list
+            l.add(toList(nums));
         } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (tempList.contains(nums[i])) {
-                    continue; // Skip if element already exists in the current permutation
-                }
-                tempList.add(nums[i]);
-                backtrack(result, tempList, nums);
-                tempList.remove(tempList.size() - 1);
+            // Loop through the array from the current index to the end
+            for (int i = start; i < nums.length; i++) {
+                // Swap the current index with the start index to generate a new permutation
+                swap(i, start, nums);
+                
+                // Recursively generate permutations for the remaining part of the array
+                backtrack(l, nums, start + 1);
+                
+                // Swap back to restore the original array state (backtrack)
+                swap(i, start, nums);
             }
         }
     }
+    
+    // Convert an array of integers to a list of integers
+    public List<Integer> toList(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        
+        // Add each element of the array to the list
+        for (int i : nums) {
+            res.add(i);
+        }
+        
+        // Return the list
+        return res;
+    }
+    
+    // Swap two elements in the array
+    public void swap(int i, int j, int[] nums) {
+        int temp = nums[i];  // Store the value at index i
+        nums[i] = nums[j];   // Assign the value at index j to index i
+        nums[j] = temp;      // Assign the stored value to index j
+    } 
 }
