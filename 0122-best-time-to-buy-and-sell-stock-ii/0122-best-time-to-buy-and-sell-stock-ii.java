d0baa060695cce8bce0426f0e89e320eebf1cdf2
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-26 01:26:28 +0530

    Time: 1 ms (90.20%), Space: 46 MB (21.09%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,12 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit=0;
+        //compare with next element and find difference
+        //add difference to result
+        //ultimately its best deal done
+        int profit =0;
         for(int i=1; i<prices.length; i++){
-            if(prices[i-1]<prices[i]){
+            if(prices[i]>prices[i-1]){
                 profit+=prices[i]-prices[i-1];
             }
         }
         return profit;
-        
     }
 }
