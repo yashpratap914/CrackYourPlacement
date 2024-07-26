17a4a2cfca36270f60868775c3eb19e8e5530f69
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-27 02:39:36 +0530

    Time: 5 ms (57.33%), Space: 45 MB (14.44%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,39 +1,22 @@
 class Solution {
     public int romanToInt(String s) {
-        int result = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            
-            if (currentChar == 'I') {
-                if (i + 1 < s.length() && (s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'X')) {
-                    result -= 1;
-                } else {
-                    result += 1;
-                }
-            } else if (currentChar == 'V') {
-                result += 5;
-            } else if (currentChar == 'X') {
-                if (i + 1 < s.length() && (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C')) {
-                    result -= 10;
-                } else {
-                    result += 10;
-                }
-            } else if (currentChar == 'L') {
-                result += 50;
-            } else if (currentChar == 'C') {
-                if (i + 1 < s.length() && (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')) {
-                    result -= 100;
-                } else {
-                    result += 100;
-                }
-            } else if (currentChar == 'D') {
-                result += 500;
-            } else if (currentChar == 'M') {
-                result += 1000;
+        Map<Character, Integer> map = new HashMap<>(); // created map with all key nd val
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = map.get(s.charAt(s.length() - 1)); // last character read
+        for (int i = s.length() - 2; i >= 0; i--) {  // loop from second last character
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) { // if val(character) on left is less than val(character) on right then subtract it from char on right
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i)); // add val(character) on i th index
             }
         }
-        
         return result;
         
     }
