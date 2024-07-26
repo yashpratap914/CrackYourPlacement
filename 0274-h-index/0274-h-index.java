289beb806b59628f69714c557ed297f5ad73b02d
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-26 14:52:49 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (24.81%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] arr = new int[n+1];
+        
+        for(int c: citations){
+            if(c>n){
+                arr[n]++;
+            }
+            else{
+                arr[c]++;
+            }
+        }
+        
+        int count = 0;
+        for(int i=n; i>=0; i--){
+            count += arr[i];
+            if(count>=i){
+                return i;
+            }
+            
+        }
+        return 0;
+        
+        
+    }
+}
