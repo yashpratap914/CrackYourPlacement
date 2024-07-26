3f791e29d17e2b496bd68dae6078b42fe35df6ec
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-27 02:33:14 +0530

    Time: 1 ms (69.22%), Space: 45.9 MB (69.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,28 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-        int[] leftmax  = new int[n];
-        leftmax[0] = height[0];
-        for(int i=1; i<n; i++){
-            leftmax[i] = Math.max(leftmax[i-1],height[i]);
-        }
-        int[] rightmax = new int[n];
-        rightmax[n-1] = height[n-1];
+    public int trap(int[] height) { // 3 1 2 4 0 1 3 2
+        //making auxilary arr left having (max val from lhs)-> highest bar 
+        int[] left = new int[height.length]; // 3 3 3 4 4 4 4 4
         
-        for(int i=n-2; i>=0; i--){
-            rightmax[i] = Math.max(height[i], rightmax[i+1]);
-        }
-        int ans=0;
+        //making auxilary arr right having max (val from rhs) -> highest bar 
+        int[] right = new int[height.length]; // 4 4 4 4 3 3 3 2
         
-        for(int i=0; i<n; i++){
-            ans += Math.min(leftmax[i], rightmax[i])-height[i];
+        left[0] = height[0];  // for left array
+
+        for (int i = 1; i < height.length; i++) {
+            left[i] = Math.max(left[i - 1], height[i]);
         }
-        return ans;
+
+        right[height.length - 1] = height[height.length - 1]; // for right array
+        for (int i = height.length - 2; i >= 0; i--) {
+            right[i] = Math.max(right[i + 1], height[i]);
+        }
         
+        int sum =0;
+        
+        for(int i=0; i<height.length; i++){
+            sum += ( Math.min(left[i],right[i])-height[i]); //minimum of left and right while subtracting height
+        }
+        return sum; // amount of water collected
         
     }
 }
