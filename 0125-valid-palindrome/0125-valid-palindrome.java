131e7f9b4586aa3e5ac8cb7ee1a1929c1db08d04
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-28 01:49:31 +0530

    Time: 2 ms (99.04%), Space: 43 MB (66.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int p1=0, p2=s.length()-1;
-        while(p1<=p2){
-            char c1 = s.charAt(p1);
-            char c2 = s.charAt(p2);
-            if(Character.isLetterOrDigit(c1)== false) p1++;
-            else if(Character.isLetterOrDigit(c2)== false) p2--;
-            else{
-                if(Character.toLowerCase(c1) != Character.toLowerCase(c2)){
-                    return false;
-                }
+        // convert string to lower case
+        //must be alphanumeric
+        // condition
+        int p1 =0;
+        int p2 = s.length()-1;
+        s = s.toLowerCase();
+        while(p1<p2){
+            char ch1 = s.charAt(p1);
+            char ch2 = s.charAt(p2);
+            
+            if(!(ch1>='a' && ch1<='z' || ch1>='0' && ch1<='9')){
                 p1++;
-                p2--;
-                
+                continue;
             }
+            if(!(ch2>='a' && ch2<='z' || ch2>='0' && ch2<='9')){
+                p2--;
+                continue;
+            }
+            if(ch1!=ch2){
+                return false;
+            }
+            p1++;
+            p2--;
         }
         return true;
-        //TC:O(n), SC:O(1)
     }
 }
