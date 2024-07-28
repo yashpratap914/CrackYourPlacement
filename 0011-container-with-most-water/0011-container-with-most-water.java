caddf5a61dc403353fc2fb6032fb78c8ec73d545
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-28 06:02:12 +0530

    Time: 2 ms (98.84%), Space: 58.1 MB (25.95%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,22 +1,23 @@
 class Solution {
     public int maxArea(int[] height) {
-        int st = 0;
-        int end = height.length -1;
-        int amt =0;
-        int maxamt =0;
-        while(st<end){
-            if(height[st]<height[end]){
-                amt = height[st]*(end - st);
-                st++;
+        int st = 0; //start line
+        int end = height.length -1; // end line
+        int amt =0; // amount 
+        int maxamt =0; // maximum amount
+        while(st<end){ // two pointer approach
+            if(height[st]<height[end]){  //height(start line) < height(end line)
+                amt = height[st]*(end - st); // area -> amount-> [width(end - start) * min(height)]
+                st++; //to increase height of start line -> maximise amount
             }else{
-                amt = height[end]*(end - st);
-                end--;
+                amt = height[end]*(end - st); // if height(end line) is less
+                end--; // to increase height of end line -> maximise amount
             }
-            if(amt>maxamt){
+            if(amt>maxamt){ // new amount compared and replaced if more
                 maxamt = amt;
             }
         }
         
         return maxamt;
+        
     }
 }
