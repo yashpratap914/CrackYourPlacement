5d6819835e3adcf6c10b5d08562f07bcc26e69c7
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-07-29 02:07:20 +0530

    Time: 12 ms (72.17%), Space: 45.2 MB (76.62%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,37 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        int k=words.length * words[0].length();
-        int ws=0;
-        int we=0;
-        List<Integer> ans=new ArrayList<>();
-        StringBuilder str=new StringBuilder();
-        while(we<s.length()){
-            str.append(s.charAt(we));
-            if(str.length()==k){
-                if(isValid(str.toString(),words)){
-                    ans.add(ws);
+        List<Integer> ans = new ArrayList<>();
+        int n = words.length;
+        int m = words[0].length();
+        int totalLength = m * n;
+
+        if (s.length() < totalLength) {
+            return ans;
+        }
+
+        HashMap<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        for (int i = 0; i < m; i++) {
+            int left = i;
+            int count = 0;
+            HashMap<String, Integer> seen = new HashMap<>();
+
+            for (int j = i; j <= s.length() - m; j += m) {
+                String word = s.substring(j, j + m);
+                if (wordCount.containsKey(word)) {
+                    seen.put(word, seen.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (seen.get(word) > wordCount.get(word)) {
+                        String leftWord = s.substring(left, left + m);
+                        seen.put(leftWord, seen.get(leftWord) - 1);
+                        left += m;
+                        count--;
+                    }
+
+                    if (count == n) {
+                        ans.add(left);
+                    }
+                } else {
+                    seen.clear();
+                    count = 0;
+                    left = j + m;
                 }
-                str.delete(0,1);
-                ws+=1;
             }
-            we++;
         }
         return ans;
-        
-    }
-    public boolean isValid(String s,String[] words){
-        int l=words[0].length();
-        HashMap<String, Integer> count=new HashMap<>();
-        for (String word : words) {
-            count.put(word,count.getOrDefault(word, 0) + 1);
-        }
-        for (int i=0;i<s.length();i+=l) {
-            String sub = s.substring(i,i+l);
-            if (!count.containsKey(sub) || count.get(sub) <= 0) {
-                return false;
-            }
-            count.put(sub,count.get(sub)-1);
-        }
-        return true;
     }
 }
