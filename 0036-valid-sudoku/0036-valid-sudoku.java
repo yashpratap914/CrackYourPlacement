ffb7f6c7af9cd5a819ee4e664a58b9e4e6e6c238
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-08-01 09:37:53 +0530

    Time: 13 ms (29.78%), Space: 45.1 MB (8.04%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,73 +1,31 @@
 class Solution {
-
     public boolean isValidSudoku(char[][] board) {
-        //neetcode solution, slightly modified
-
-        //a set of the characters that we have already come across (excluding '.' which denotes an empty space)
-        Set<Character> rowSet = null;
-        Set<Character> colSet = null;
-
-
-        for (int i = 0; i < 9; i++) {
-            //reinitialize the sets so we don't carry over found characters from the previous run
-            rowSet = new HashSet<>();
-            colSet = new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                char r = board[i][j];
-                char c = board[j][i];
-                if (r != '.'){
-                    if (rowSet.contains(r)){
-                        return false;
-                    } else {
-                        rowSet.add(r);
-                    }
-                }
-                if (c != '.'){
-                    if (colSet.contains(c)){
-                        return false;
-                    } else {
-                        colSet.add(c);
-                    }
-                }
-            }
-        }
-
-        //block
-        //loop controls advance by 3 each time to jump through the boxes
-        for (int i = 0; i < 9; i = i + 3) {
-            for (int j = 0; j < 9; j = j + 3) {
-                //checkBlock will return true if valid
-                if (!checkBlock(i, j, board)) {
-                    return false;
-                }
-            }
-        }
-        //passed all tests, therefore valid board
-        return true;
-    }
-
-    public boolean checkBlock(int idxI, int idxJ, char[][] board) {
-        Set<Character> blockSet = new HashSet<>();
-        //if idxI = 3 and indJ = 0
-        //rows = 6 and cols = 3
-        int rows = idxI + 3;
-        int cols = idxJ + 3;
-        //and because i initializes to idxI but only goes to rows, we loop 3 times (once for each row)
-        for (int i = idxI; i < rows; i++) {
-            //same for columns
-            for (int j = idxJ; j < cols; j++) {
-                if (board[i][j] == '.') {
-                    continue;
-                }
+        // Brute Force: each row:(1-9) -> no repetition
+        // each column: each column:(1-9) -> no repetition
+        // Each nine 3X3 sub boxes -> (1-9) -> no repetition
+        HashSet<String> set = new HashSet<>();
+        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                char number = board[i][j];
                 
-                if (blockSet.contains(board[i][j])) {
-                    return false;
+                if(number != '.'){
+                    String str1 = number + " in row " + i;
+                    String str2 = number + " in column " + j;
+                    String str3 = number + " in block " + i/3 + "-" + j/3;
+                    
+                    if(set.contains(str1) || set.contains(str2) || set.contains(str3)){
+                        return false;
+                    }else{
+                        set.add(str1);
+                        set.add(str2);
+                        set.add(str3);
+                    }
+                    
                 }
-
-                blockSet.add(board[i][j]);
             }
         }
-
+        
         return true;
     }
 }
