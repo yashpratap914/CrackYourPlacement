e48e86506fc003025131de3e32d6f3c85bab5092
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-08-01 16:29:45 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (60.81%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,53 +1,23 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean hasFirstRow = false;
-        boolean hasFirstCol = false;
-        for(int c=0; c<matrix[0].length; c++){  //first row
-            if(matrix[0][c]==0){
-                hasFirstRow = true;
-                break;
-            }
-            
-        }
-        for(int r =0; r<matrix.length; r++){ //first col
-            if(matrix[r][0] == 0){
-                hasFirstCol = true;
-                break;
-            }
-            
-        }
-        //preprocessing
-        for(int r =1; r<matrix.length; r++){
-            for(int c =1; c<matrix[0].length; c++){
-                if (matrix[r][c] == 0){
-                    matrix[r][0]=0;
-                    matrix[0][c]=0;
-                    
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int[] row = new int[rows]; // new 1 d arrays to mark row with zero
+        int[] column = new int[columns];
+        for(int i =0; i < rows; i++) {
+            for(int j=0; j < columns; j++) {
+                if(matrix[i][j] == 0) { // marked box with 0 as ele // 1, 1
+                    row[i] = -1; // row[1]
+                    column[j] = -1; // col[1]
                 }
             }
         }
-        for(int c =1; c<matrix[0].length; c++){ //1st row
-            if(matrix[0][c]==0){
-                nullifyCol(matrix,c);
+        for(int i =0; i < rows; i++) {
+            for(int j =0; j< columns; j++) {
+                if(row[i]==-1 || column[j]==-1){//traverse again and mark those with negative ele as 0 while traversing
+                    matrix[i][j] = 0;
+                }
             }
         }
-        for(int r =1; r< matrix.length; r++){  // 1st column
-            if(matrix[r][0]== 0){
-                nullifyRow(matrix, r);
-            }
-            
-        }
-        if (hasFirstRow) nullifyRow(matrix, 0);
-        if (hasFirstCol) nullifyCol(matrix, 0);
     }
-    public void nullifyRow(int[][] matrix, int row){
-        for(int c =0; c<matrix[0].length; c++){
-            matrix[row][c] = 0;
-        }
-    }
-    public void nullifyCol(int[][] matrix, int col){
-        for(int r =0; r<matrix.length; r++){
-            matrix[r][col] = 0;
-        }
-    }//TC:O(2R)+O(2C)+O(R*C)+O(R*C); SC:0(1)
 }
