92061fb3b7761bf57b3cbb1941657d1839f7997a
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-08-01 15:52:34 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (12.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -6,8 +6,8 @@ class Solution {
     }
     public void transpose(int[][] arr){
         int n = arr.length;
-        for(int i =0; i<n; i++){
-            for(int j=0; j<i; j++){
+        for(int i =0; i<n; i++){ // rows : start -> end
+            for(int j=0; j<i; j++){ // swap non equal(non diagnol) i and j(transpose)
                 int temp = arr[i][j];
                 arr[i][j] = arr[j][i];
                 arr[j][i] = temp;
@@ -16,12 +16,12 @@ class Solution {
         
     }
     public void reverserow(int[][] arr){
-        for(int r=0; r<arr.length; r++){
-            int left =0;
-            int right = arr.length-1;
+        for(int r=0; r<arr.length; r++){ // row
+            int left =0;  
+            int right = arr.length-1; // 2 pointer(left(first) and right(last))
             while(left<right){
                 int temp = arr[r][left];
-                arr[r][left] = arr[r][right];
+                arr[r][left] = arr[r][right];  // swap of first nd last until middle -> reverse
                 arr[r][right]= temp;
                 left++;
                 right--;
