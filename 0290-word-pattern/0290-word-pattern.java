c13c43c8c475dc05002e66bb39cf8c7a90caa4f4
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-08-03 00:20:54 +0530

    Time: 1 ms (83.02%), Space: 41.2 MB (58.85%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,27 +1,32 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] sArray = s.split("\s");
-        if(sArray.length != pattern.length()) {
+        HashMap<Character,String> mp = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        
+        String[] arr = s.split(" "); //split word string into array
+        int n = arr.length;
+        if(n!=pattern.length()){ //word arrlength == string length 
             return false;
         }
-        
-        HashMap<Character,String> charToWord = new HashMap<>();
-        HashMap<String,Character> wordToChar = new HashMap<>();
-        
-        for (int i = 0; i < pattern.length(); i++) {
-        
-            if(charToWord.containsKey(pattern.charAt(i)) && !charToWord.get(pattern.charAt(i)).equals(sArray[i])) {
-                return false;
-            }
+        for(int i=0; i<n; i++){
+            String word = arr[i];
+            char ch = pattern.charAt(i);
             
-            if(wordToChar.containsKey(sArray[i]) && !wordToChar.get(sArray[i]).equals(pattern.charAt(i))) {
-                return false;
+            if(mp.containsKey(ch)){ //if char is already in map 
+                if(!mp.get(ch).equals(word)){ //and current value not equal to value already present
+                    return false;
+                }
             }
-            
-            charToWord.put(pattern.charAt(i),sArray[i]);
-            wordToChar.put(sArray[i],pattern.charAt(i));
+            else{  //if char is not already in map 
+                if(set.contains(word)){ //val associated with else
+                    return false;
+                }
+                else{ // if totally new val-word pair add
+                    mp.put(ch, word);
+                    set.add(word);
+                }
+            }
         }
         return true;
-        
     }
 }
