0052e073f539cafcbe8e6a89252ad9fbba19bd0e
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-08-03 19:39:41 +0530

    Time: 1 ms (63.13%), Space: 41 MB (13.13%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,37 +1,27 @@
-import java.util.LinkedList;
-
 class Solution {
     public boolean isHappy(int n) {
-        
-        int slow = n;
-        int fast = n;
-//while loop is not used here because initially slow and 
-//fast pointer will be equal only, so the loop won't run.
-        do {
-//slow moving one step ahead and fast moving two steps ahead
+        // Create a set to keep track of the numbers that have already been seen
+        Set<Integer> seen = new HashSet<>();
+        // Loop until either n becomes 1 (in which case n is a happy number) 
+        //                         OR
+        //n is seen again (in which case n is not a happy number)
 
-            slow = square(slow);
-            fast = square(square(fast));
-        } while (slow != fast);
-
-//if a cycle exists, then the number is not a happy number
-//and slow will have a value other than 1
-
-        return slow == 1;
-    }
-    
-//Finding the square of the digits of a number
-
-    public int square(int num) {
-        
-        int ans = 0;
-        
-        while(num > 0) {
-            int remainder = num % 10;
-            ans += remainder * remainder;
-            num /= 10;
+        while (n != 1 && !seen.contains(n)) {
+            // Add n to the set of seen numbers
+            seen.add(n);
+            // Compute the sum of the squares of the digits of n
+            int sum = 0;
+            while (n != 0) {
+                int digit = n % 10; // last dig
+                sum += digit * digit; // square of dig
+                n /= 10;  // rest dig
+            }
+            // Update n with the sum of the squares of its digits
+            n = sum;
         }
         
-        return ans;
+        // If n is 1, it is a happy number; otherwise, it is not
+        return n == 1;
+        
     }
 }
