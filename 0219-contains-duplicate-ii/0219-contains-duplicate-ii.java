5071370c08b91a6214bd976030c6a5546230ee05
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-08-03 19:48:36 +0530

    Time: 18 ms (72.55%), Space: 58.1 MB (14.13%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,13 +1,27 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> mp=new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(mp.containsKey(nums[i])){
-                if(Math.abs(i-mp.get(nums[i])) <= k) return true;
+        // Create a HashMap to store the value and its most recent index
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        // Iterate through the array
+        for (int currinx = 0; currinx < nums.length; currinx++) {
+            // Check if the current value already exists in the HashMap
+            if (mp.containsKey(nums[currinx])) {
+                // Retrieve the index of the last occurrence of the current value
+                int pastinx = mp.get(nums[currinx]);
+
+                // Check if the distance between the current index and the last index is within the allowed range
+                if (Math.abs(pastinx - currinx) <= k) {
+                    // If the condition is met, return true
+                    return true;
+                }
             }
-            mp.put(nums[i], i);
+
+            // Update the HashMap with the current value and its index
+            mp.put(nums[currinx], currinx);
         }
+
+        // If no such pair is found, return false
         return false;
-        
     }
 }
