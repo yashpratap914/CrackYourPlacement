64e7f1fc0a74ca73ad4e33c392501c8e18e19414
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-08-17 12:06:21 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (84.66%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,29 +1,28 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        int ones = 0; //count of students who prefer type1
-        int zeros = 0; //count of students who prefer type0
-		
-        for(int stud : students){
-            if(stud == 0) zeros++;
-            else ones++;
-        }
-        
-        // for each sandwich in sandwiches
-        for(int sandwich : sandwiches){
-            if(sandwich == 0){  // if sandwich is of type0
-                if(zeros == 0){ // if no student want a type0 sandwich
-                    return ones;
-                }
-                zeros--;
+        int circswt = 0;
+        int sqswt = 0;
+        for(int ch : students){  // freq of sandwiches needed 
+            if(ch==0){
+                circswt++;
+            }else{
+                sqswt++;
             }
-            else{  // if sandwich is of type1
-                if(ones == 0){  // if no student want a type1 sandwich 
-                    return zeros;
-                }
-                ones--;
+        }
+        for(int swt : sandwiches){
+            if(swt == 0 && circswt ==0){  //circular ki chahat wale nhi hain
+                return sqswt;
+            }
+            if(swt == 1 && sqswt ==0){  // square ki chahat wale nhi hain
+                return circswt;
+            }
+            if(swt == 0){   // agar circular aaya toh 
+                circswt--;
+            }
+            else{            // agar square aaya toh 
+                sqswt--;
             }
         }
         return 0;
-        
     }
 }
