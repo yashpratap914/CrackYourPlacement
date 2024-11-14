03d2b6d8cd404fbf6aa3ec727f6f7bcb5069dfd2
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-11-14 08:29:11 +0530

    Time: 83 ms (45.00%), Space: 56.6 MB (52.26%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,40 +1,21 @@
 class Solution {
-    public int minimizedMaximum(int n, int[] quantities) {
-        int st =1; //to avoid div by zero
-        int end = Integer.MIN_VALUE;
-        
-        for(int i =0; i<quantities.length; i++){
-            if(quantities[i]>end){
-                end= quantities[i];
-            }
+    public boolean isPossible(int x, int[] quantities, int n) {
+        double sum = 0;
+        for (int u : quantities) {
+            sum += Math.ceil((double) u / x);
         }
-        int ans =-1;
-        
-        while(st<=end){
-            int mid = st + (end-st)/2;
-            if(isokay(quantities, mid, n)){
-                ans = mid;
-                end = mid-1;
-            }else{
-                st = mid+1;
-            }
-        }
-        return ans;
+        return sum > n;
     }
-    
-    boolean isokay(int[] quantities, int maxprod, int n){
-        
-        int storecount =0;
-        for(int i=0; i<quantities.length; i++){
-            storecount += quantities[i]/maxprod;
-            if(quantities[i]%maxprod!=0){
-                storecount+=1;
-            }
-            if(storecount>n){
-                return false;
-            }
+
+    public int minimizedMaximum(int n, int[] quantities) {
+        int left = 1, right = 100000;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (isPossible(mid, quantities, n))
+                left = mid + 1;
+            else
+                right = mid;
         }
-        return true;
-        
+        return left;
     }
 }
