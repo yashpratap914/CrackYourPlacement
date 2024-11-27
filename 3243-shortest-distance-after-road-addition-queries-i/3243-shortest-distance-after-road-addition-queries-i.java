77b1038e344570de326f48e85058893aa4e2f828
Yash Pratap Singh <122447300+yashpratap914@users.noreply.github.com>
2024-11-27 19:45:41 +0530

    Time: 177 ms (39.61%), Space: 45.8 MB (25.25%) - LeetHub

---
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.java
@@ -0,0 +1,54 @@
+class Solution {
+    private int bellmanFordHelper(List<List<Integer>> list) {
+        int n = list.size();
+        
+        int[] distance = new int[n];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[0] = 0;
+
+        // for (int i = 0; i < n - 1; i++) {
+        //     for (int u = 0; u < n; u++) {
+        //         for (int v : list.get(u)) {
+        //             if (distance[u] + 1 < distance[v]) {
+        //                 distance[v] = distance[u] + 1;
+        //             }
+        //         }
+        //     }
+        // }
+        
+         Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            for (int v : list.get(u)) {
+                if (distance[u] + 1 < distance[v]) {
+                    distance[v] = distance[u] + 1;
+                    queue.add(v);
+                }
+            }
+        }
+
+        return distance[n - 1];
+    }
+    
+    
+    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n - 1; i++) {
+            list.get(i).add(i + 1);
+        }
+        
+        int[] res = new int[queries.length];
+        int i=0;
+        for (int[] query:queries) {
+            list.get(query[0]).add(query[1]);
+            res[i++] = bellmanFordHelper(list);
+        }
+
+        return res;
+    }
+}
